25e3b14dccdb06351194edf1e5b90a448b493a24
Chris Vaz <76993434+chris-vaz@users.noreply.github.com>
2023-01-03 15:00:02 +0300

    Improved CRUD

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
-// const methodOverride = require('method-override');
+const methodOverride = require('method-override');
 const Campground = require('./models/campground');
 
 mongoose.set('strictQuery', false);
@@ -18,7 +18,7 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'))
 
 app.use(express.urlencoded({ extended: true }));
-// app.use(methodOverride('_method'));
+app.use(methodOverride('_method'));
 
 
 app.get('/', (req, res) => {
